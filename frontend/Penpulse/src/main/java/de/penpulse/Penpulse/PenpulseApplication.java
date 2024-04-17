35bcb17e3c43925529726d64fe7db8d2a826e824
baheersbi <baseer.baheer@berlinsbi.com>
2024-04-17 18:02:44 +0200

    Added Frontend

---
+++ frontend/Penpulse/src/main/java/de/penpulse/Penpulse/PenpulseApplication.java
@@ -0,0 +1,13 @@
+package de.penpulse.Penpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenpulseApplication.class, args);
+	}
+
+}
---
+++ frontend/Penpulse/src/test/java/de/penpulse/Penpulse/PenpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.penpulse.Penpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
